acc8f4baccbd616437537b7bd1a921ae6e1f79ec
Dishi Zhu <dishi.zhu@mail.mcgill.ca>
2020-11-08 17:22:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import {
+  Modal,
+  Button,
+  Input,
+  Typography,
+  Form,
+  Select,
+  DatePicker,
+} from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const { Title } = Typography;
+const { Option } = Select;
+
+class App extends React.Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Title>Independent Study</Title>
+        <Button type="primary" onClick={this.showModal}>
+          Get Sentiment Analysis
+        </Button>
+        <Modal
+          title="Select Specifications"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={false}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Form
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 14 }}
+            layout="horizontal"
+          >
+            <Form.Item label="Ticker">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Data source">
+              <Select>
+                <Option value="twitter">Twitter</Option>
+                <Option value="reddit">Reddit</Option>
+                <Option value="10q">10Q</Option>
+                <Option value="10k">10K</Option>
+                <Option value="googleFinance">Google Finance</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="From">
+              <DatePicker />
+            </Form.Item>
+            <Form.Item label="To">
+              <DatePicker />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary">Submit</Button>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default App;
